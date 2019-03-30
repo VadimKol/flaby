d0906a9e67595b4c962abbc7dfe0deec7141cc47
VadimKol <47818256+VadimKol@users.noreply.github.com>
2019-03-30 11:05:21 +0300

    done with psd template
    template before adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import "./import/blocks.js";
+
+$(document).ready(function(){
+    //no js
+    $(".slidenav").show();
+    //no js
+    $('.slider').slick({
+        prevArrow: $(".slidenav__btn").first(),
+        nextArrow: $(".slidenav__btn").last(),
+        draggable: false
+    });
+  });
